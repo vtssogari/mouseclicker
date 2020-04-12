84f47b111cc6a09d8ed82d394e1f2af1394f790a
Kun <vtssogari@gmail.com>
2020-04-11 20:35:25 -0400

    Add project files.

---
+++ mouseclicker/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace mouseclicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Location = new System.Drawing.Point(51, 23);
+            this.status.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(76, 13);
+            this.status.TabIndex = 0;
+            this.status.Text = "Wake up call: ";
+            this.status.Click += new System.EventHandler(this.status_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(203, 57);
+            this.Controls.Add(this.status);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Don\'t Sleep";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label status;
+    }
+}
+
---
+++ mouseclicker/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace mouseclicker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
+        int alarmCounter = 1;
+        bool exitFlag = false;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        
+            
+        }
+
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            myTimer.Tick += new EventHandler(TimerEventProcessor);
+
+            // Sets the timer interval to 5 seconds.
+            myTimer.Interval = 2000;
+            myTimer.Start();
+
+            // Runs the timer, and raises the event.
+            while (exitFlag == false)
+            {
+                // Processes all the events in the queue.
+                Application.DoEvents();
+            }
+        }
+
+        // This is the method to run when the timer is raised.
+        private void TimerEventProcessor(Object myObject, EventArgs myEventArgs)
+        {
+            this.alarmCounter += 1;
+            MouseEvent(MouseEventFlags.LeftDown | MouseEventFlags.LeftUp);
+            status.Text = "wake up call: " + alarmCounter;
+        }
+
+        [Flags]
+        public enum MouseEventFlags
+        {
+            LeftDown = 0x00000002,
+            LeftUp = 0x00000004,
+            MiddleDown = 0x00000020,
+            MiddleUp = 0x00000040,
+            Move = 0x00000001,
+            Absolute = 0x00008000,
+            RightDown = 0x00000008,
+            RightUp = 0x00000010
+        }
+
+        [DllImport("user32.dll", EntryPoint = "SetCursorPos")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetCursorPos(int x, int y);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetCursorPos(out MousePoint lpMousePoint);
+
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+        public static void SetCursorPosition(int x, int y)
+        {
+            SetCursorPos(x, y);
+        }
+
+        public static void SetCursorPosition(MousePoint point)
+        {
+            SetCursorPos(point.X, point.Y);
+        }
+
+        public static MousePoint GetCursorPosition()
+        {
+            MousePoint currentMousePoint;
+            var gotPoint = GetCursorPos(out currentMousePoint);
+            if (!gotPoint) { currentMousePoint = new MousePoint(0, 0); }
+            return currentMousePoint;
+        }
+
+        public static void MouseEvent(MouseEventFlags value)
+        {
+            MousePoint position = GetCursorPosition();
+
+            mouse_event
+                ((int)value,
+                 position.X,
+                 position.Y,
+                 0,
+                 0)
+                ;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MousePoint
+        {
+            public int X;
+            public int Y;
+
+            public MousePoint(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+
+        private void status_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
